65ea4743cc8800a1a5db1775cbd10d0c737440c5
Mohammad Alrefai <malrefai@nyit.edu>
2018-10-25 13:38:07 +0300

    Organize fields

--- board/models/post.py
+++ board/models/post.py
@@ -6,10 +6,10 @@ from .topic import Topic
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(
-        Topic, related_name="posts", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
+    topic = models.ForeignKey(
+        Topic, related_name="posts", on_delete=models.CASCADE)
     created_by = models.ForeignKey(
         User, related_name="posts", on_delete=models.CASCADE)
     updated_by = models.ForeignKey(
