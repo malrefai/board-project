690721d2a0a73f9076df9b40fb99178cf4196d6c
Mohammad Alrefai <malrefai@nyit.edu>
2018-10-25 13:20:02 +0300

    Add specs for topic model

---
+++ board/tests/models/test_topic.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.db import models
+
+from ..factories import TopicFactory
+
+
+class TopicModelTest(TestCase):
+    """
+    This class defines the test suite for the board model.
+    """
+
+    topic = None
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        TopicModelTest.topic = TopicFactory.build()
+
+    def test_subject_field(self):
+        field = TopicModelTest.topic._meta.get_field("subject")
+        self.assertIsInstance(field, models.CharField)
+        self.assertEqual(field.max_length, 255)
+
+    def test_last_update_field(self):
+        field = TopicModelTest.topic._meta.get_field("last_update")
+        self.assertIsInstance(field, models.DateTimeField)
+        self.assertTrue(field.auto_now_add)
+
+    def test_board_field(self):
+        field = TopicModelTest.topic._meta.get_field("board")
+        self.assertIsInstance(field, models.ForeignKey)
+
+    def test_starter_field(self):
+        field = TopicModelTest.topic._meta.get_field("starter")
+        self.assertIsInstance(field, models.ForeignKey)
+
+    def test_model_returns_readable_representation(self):
+        """Test a readable string is returned for the model instance."""
+        self.assertEqual(
+            str(TopicModelTest.topic), TopicModelTest.topic.subject)
